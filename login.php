583f95bc71963e6908a4d8d1a0886ee4bce9b7a1
KiranaAdityaMoza <kiranamoza4@gmail.com>
2026-05-03 13:26:10 +0700

    Add files via upload
    dashboard user dan fitur booking

--- login.php
+++ login.php
@@ -10,19 +10,21 @@ $query->bind_param("s",$email);
 $query->execute();
 $result = $query->get_result();
 
-if($result->num_rows>0){
-    $user=$result->fetch_assoc();
+if($result->num_rows > 0){
+    $user = $result->fetch_assoc();
 
     if($password == $user['password']){
-        $_SESSION['nama']=$user['name'];
-        $_SESSION['email']=$user['email'];
-        $_SESSION['role']=$user['role'] ?? 'user';
+        $_SESSION['nama'] = $user['name'];
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['role'] = $user['role'] ?? 'user';
 
-        if($_SESSION['role']==='admin'){
-            header("Location: admin.html");
-        }else{
-            header("Location: dashboard.php");
-        }
+        // Cari kode ini di login.php
+    if($_SESSION['role'] === 'admin'){
+        header("Location: admin.html");
+    } else {
+        // GANTI baris ini agar menuju ke file baru kamu
+        header("Location: user.html"); 
+    }
         exit;
     }
 }
